d8adacd09ed0d32095adc4e67810755c2ab0f05f
Nhoni <nhmbemba@gmail.com>
2023-11-09 16:37:18 +0100

    🎉

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./index.css";
+
+import Footer from "./components/Footer";
+
+import HeroSection from "./components/HeroSection";
+import ContactSection from "./components/ContactSection";
+import AboutSection from "./components/AboutSection";
+import FactSection from "./components/FactSection";
+import SkillsSection from "./components/SkillSection";
+import ResumeSection from "./components/ResumeSection";
+import ServiceSection from "./components/ServiceSection";
+import TestimonialSection from "./components/TestimonialSection";
+import Navbar from "./components/NavBar";
+import PortfolioSection from "./components/PortfolioSection";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <HeroSection />
+      <AboutSection />
+      <FactSection />
+      <SkillsSection />
+      <ResumeSection />
+      <PortfolioSection />
+      <ServiceSection />
+      <TestimonialSection />
+      <ContactSection />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
